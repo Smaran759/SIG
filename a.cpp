20a2d11459182b5d87b1cef4e0bbac3a6b6ff859
Smaran759 <smaran.dandekar753@gmail.com>
2026-02-06 18:15:46 +0530

    Function for multiplying

---
+++ a.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int multiply(mat1, mat2){
+    int mat[][] = [[0,0,0],[0,0,0],[0,0,0]];
+    for(int i = 0; i<3;i++){
+        for(int j = 0; j<3;j++){
+            for(int k =0; k<3;k++){
+                mat3[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+    printmat(mat3)
+}
+
+int main(){
+
+}
